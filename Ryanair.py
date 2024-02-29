130d883e239f858d0c240f31ef8565d86c568b2d
Taha Enayat <ta.enayat@gmail.com>
2024-02-29 13:51:11 +0100

    for loop is now clear using functions

--- Ryanair.py
+++ Ryanair.py
@@ -148,64 +148,12 @@ options.add_argument('--disable-dev-shm-usage')
 #################### FIND ALL ORIGIN-DESTINATION PAIRS ####################
 ###########################################################################
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-driver.get("http://ryanair.com")
-
-driver.maximize_window()
-
-## accept cookies
-driver.find_element("xpath", '//*[@id="cookie-popup-with-overlay"]/div/div[3]/button[3]').click()
-
-## one-way trip
-# one_way_flight = driver.find_element(By.ID, value='ry-radio-button--0')
-# actions = ActionChains(driver)
-# actions.move_to_element(one_way_flight).click().perform()
-
-## origins
-driver.find_element('xpath', '//*[@id="input-button__departure"]').click()
-# driver.execute_script("arguments[0].click();", origins)
-
-# countries = driver.find_elements('xpath', '//fsw-countries[contains(@class,"countries__country")]')
-origins = driver.find_elements('xpath', '//span[contains(@class,"countries__country-inner")]')
-print(len(origins))
-country = origins[1]
-country_name = country.get_attribute("innerHTML")
-print(country_name)
-# driver.execute_script("arguments[0].click();", country)
-country.click()
-origin_airports_clickable = driver.find_elements(By.TAG_NAME, 'fsw-airport-item')
-origin_airports = driver.find_elements('xpath', '//fsw-airport-item//span[@data-ref="airport-item__name"]')
-len(origin_airports)
-origin_airport = origin_airports[0]
-origin_airport_code = origin_airport.get_attribute("data-id")
-print(origin_airport_code)
-origin_airport_name = re.sub('[\W_]+', '', origin_airports[0].get_attribute("innerHTML"))
-# origin_airports_clickable[0].click()
-driver.execute_script("arguments[0].click();", origin_airports_clickable[0])
-
-
-
-## destination
-destinations =  driver.find_elements('xpath', '//span[contains(@class,"countries__country-inner")]')
-print(len(destinations))
-destination_country = destinations[3]
-print(destination_country.get_attribute("innerHTML"))
-'not-available' not in destination_country.get_attribute('class')
-destination_country.click()
-destination_airports_clickable = driver.find_elements(By.TAG_NAME, 'fsw-airport-item')
-len(destination_airports_clickable)
-destination_airports = driver.find_elements('xpath', '//fsw-airport-item//span[@data-ref="airport-item__name"]')
-destination_airport = destination_airports[0]
-destination_airport_code = destination_airport.get_attribute("data-id")
-print(destination_airport_code)
-destination_airport_name = re.sub('[\W_]+', '', destination_airports[0].get_attribute("innerHTML"))
-print(destination_airport_name)
-
-
-
-
-
+def init_driver(options):
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+def accept_cookies(driver):
+    driver.find_element("xpath", '//*[@id="cookie-popup-with-overlay"]/div/div[3]/button[3]').click()
+def scroll(driver, amount):
+    ActionChains(driver).scroll_by_amount(0, 100).perform()
 def click_origin_box(driver): 
     driver.find_element('xpath', '//*[@id="input-button__departure"]').click()
 def find_countries(driver):
@@ -255,91 +203,45 @@ def append_to_list(l, origin_country_name, origin_airport_code,origin_airport_na
     l.append(dic)
 
 
-## For loop
-# df = pd.DataFrame()
+driver = init_driver(options)
+driver.get("http://ryanair.com")
+accept_cookies(driver)
+
 data_list = []
-# select origin box
-# driver.find_element('xpath', '//*[@id="input-button__departure"]').click()
 click_origin_box(driver)
 countries_len = len(find_countries(driver))
-# for origin_country in origin_countries[:2]:
+
 # for origin_idx in range(countries_len):
 for origin_idx in range(2):
     origin_country_name = origin_country_select(driver, origin_idx)
     time.sleep(1)
 
     origin_airports_clickable = find_airports_clickable(driver)
-    # origin_airports = driver.find_elements('xpath', '//fsw-airport-item//span[@data-ref="airport-item__name"]')
-    # len(origin_airports)
-
-    # trunc = 2
-    # for origin_airport, origin_airport_clickable in zip(origin_airports[:trunc], origin_airports_clickable[:trunc]):
     # for airport_idx in range(len(origin_airports_clickable)):
     for origin_airport_idx in range(2):
         origin_airport_code, origin_airport_name = airport_select(driver, origin_airport_idx)
         time.sleep(1)
 
-        # destination_countries =  find_countries(driver)
-        # for destination_country in destination_countries:
         # for destination_idx in range(countries_len):
         for destination_idx in range(2):
             destination_country =  find_countries(driver)[destination_idx]
             if is_available(destination_country):
                 desination_country_name = destination_country_select(destination_country)
                 destination_airports_clickable = find_airports_clickable(driver)
-                # destination_airports = driver.find_elements('xpath', '//fsw-airport-item//span[@data-ref="airport-item__name"]')
-                # len(destination_airports_clickable)
                 time.sleep(1)
 
-                # for destination_airport, destination_airport_clickable in zip(destination_airports[:trunc], destination_airports_clickable[:trunc]):
                 # for dest_airport_idx in range(len(destination_airports_clickable)):    
                 for dest_airport_idx in range(2):    
                     destination_airport_code, destination_airport_name = airport_select(driver, dest_airport_idx, is_origin=False)
                     
                     append_to_list(data_list)
-                    # print("final:", dic)
                     time.sleep(1)
 
 
 
-
-# ## destinations
-# destination = driver.find_element('xpath','//*[@id="input-button__destination"]')
-# destination.send_keys('Venecia Treviso')
-# airports = driver.find_elements(By.TAG_NAME, 'fsw-airport-item')
-# driver.execute_script("arguments[0].click();", airports[1])
-# driver.find_element('xpath','//*[@id="ry-tooltip-3"]/div[2]/hp-app-controls-tooltips/fsw-controls-tooltips-container/fsw-controls-tooltips/fsw-destination-container/fsw-airports/div/fsw-airports-list/div[2]/div[1]/fsw-airport-item[2]').click() # first one in the list
-
-## scroll to see better
-# ActionChains(driver).scroll_by_amount(0, 100).perform()
-
-## departure date
-# departure = driver.find_element('xpath','//*[@id="ry-tooltip-10"]/div[2]/hp-app-controls-tooltips/fsw-controls-tooltips-container/fsw-controls-tooltips/fsw-flexible-datepicker-container/fsw-datepicker/ry-datepicker-desktop/div')
-# dates = departure.find_elements(By.CLASS_NAME, 'calendar-body__cell')
-
-# for date in dates: 
-#     class_value = date.get_attribute('class')
-#     if 'calendar-body__cell--disabled' not in class_value: # available dates
-#         date_value = date.get_attribute('data-id')
-#         # print(date_value)
-#         if date_value == "2024-03-27":
-#             print(date_value)
-#             chosen_date = date
-#             break
-# chosen_date.click()
-
-
-## click search button
-# driver.find_element('xpath', '//button[contains(@class,"flight-search-widget__start-search-cta")]').click()
-
-
-# time.sleep(2)
-# fares = driver.find_elements('xpath', '//carousel-item[@class="ng-star-inserted"]')
-
-
 # scroll to see better
-ActionChains(driver).scroll_by_amount(0, 100).perform()
-driver.save_screenshot("screenshot1.png")
+scroll(driver, 100)
+# driver.save_screenshot("screenshot1.png")
 driver.quit()
 
 
