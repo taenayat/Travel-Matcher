8b2ee9e6731be8adf22517358bcb80137f98da91
Taha Enayat <ta.enayat@gmail.com>
2024-06-03 00:50:40 +0200

    better comparison of two dataframe

--- visualization.py
+++ visualization.py
@@ -13,7 +13,7 @@ def price_corrector(column: pd.Series):
 def moving_avg(column: pd.Series):
     pass
 
-price_df = pd.read_csv('AGP_TSF_20240527_20241230.csv')
+price_df = pd.read_csv('AGP_TSF_20240603_20241230.csv')
 price_df['price'] = price_corrector(price_df['price'])
 # price_df['price'] = price_df['price']
 price_df['date'] = pd.to_datetime(price_df['date'])
@@ -23,21 +23,31 @@ plt.figure()
 plt.scatter(price_df['date'], price_df['price'])
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
-plt.show()
+# plt.show()
 
-price_nextday_df = pd.read_csv('AGP_TSF_20240528_20241230.csv')
-price_nextday_df['price'] = price_corrector(price_nextday_df['price'])
-price_nextday_df['date'] = pd.to_datetime(price_nextday_df['date'])
-price_nextday_df.rename(columns=dict([(col,col+'2') for col in price_nextday_df.columns if col != 'date']), inplace=True)
+price_prevday_df = pd.read_csv('AGP_TSF_20240530_20241230.csv')
+price_prevday_df['price'] = price_corrector(price_prevday_df['price'])
+price_prevday_df['date'] = pd.to_datetime(price_prevday_df['date'])
+price_prevday_df.rename(columns=dict([(col,col+'2') for col in price_prevday_df.columns if col != 'date']), inplace=True)
 
-join_df = price_df.join(price_nextday_df.set_index('date'), on='date')[['date','price','price2']]
-join_df.drop_duplicates(inplace=True).dropna(inplace=True)
+join_df = price_df.join(price_prevday_df.set_index('date'), on='date')[['date','price','price2']]
+join_df.drop_duplicates(inplace=True)
+join_df.dropna(inplace=True)
 join_df['price_diff'] = join_df['price2'] - join_df['price']
 
 # plot_df = join_df[join_df.price_diff < 50]
 plot_df = join_df
+
+# plt.figure()
+# plt.scatter(plot_df['date'], plot_df['price_diff'])
+# plt.xticks(rotation=45, ha='right')
+# plt.tight_layout()
+# plt.show()
+
 plt.figure()
-plt.scatter(plot_df['date'], plot_df['price_diff'])
+plt.scatter(plot_df['date'], plot_df['price2'], label='previous')
+plt.scatter(plot_df['date'], plot_df['price'], label = 'now')
+plt.legend()
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
